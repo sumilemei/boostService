4c4047d25866ba7634e4402fd3e585a728f1715c
sumilemei <taozhengnj@163.com>
2026-02-22 19:57:22 +0800

    messsage: 1

--- elasticsearch/src/main/java/com/taoz/boost/iss/service/ICompleteCheck.java
+++
@@ -1,8 +0,0 @@
-package com.taoz.boost.iss.service;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface ICompleteCheck {
-    void dataCompleteCheck(Object obj);
-}
--- sourcecode/src/main/java/com/taoz/source/test/Test01.java
+++
@@ -1,7 +0,0 @@
-package com.taoz.source.test;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class Test01 {
-}
